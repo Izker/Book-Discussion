0b3ee8c82122b18653329ba6b2a0a2102f5ab206
DONGTN\Thanh <ngocdon641995@gmail.com>
2017-05-11 20:01:42 +0700

    add func delete cmt, hide, edit

--- document/Nhom01-PA4/Mookup/BookShelf/assets/js/comment.js
+++ document/Nhom01-PA4/Mookup/BookShelf/assets/js/comment.js
@@ -10,6 +10,9 @@ function comment(e){
             if( valueArea != ""){
               $("#" + areaId).val("");
               var cmt_store = $("#comment-store").clone();
+              var timeStamp = 'comment-' +Number(new Date());
+              cmt_store.attr('id',  timeStamp);
+              cmt_store.find('div.dropdown-menu a').attr('class', timeStamp);
               cmt_store.css("display", "");
               cmt_store.find("span.content").text(valueArea);
               cmt_store.find("span.nameOfUser").text($("#username").text());
@@ -27,4 +30,30 @@ function comment(e){
       $("#" + id).css("display", "none");
       $("#comments-" + id.split("-")[1]).children().css("display", "");
     }
+  }
+  function deleteCmt(e){
+    e.preventDefault();
+    var self = e.target;
+    var id = self.className;
+    $("#" + id).remove();
+  }
+
+  function editCmt(e){
+    e.preventDefault();
+    var id = e.target.className;
+    var contentText = $("#" + id).find("span.content").text();
+    var idParent = $("#" + id).parent().attr("id");
+    var idArea = idParent.split("-")[1];
+    $('textarea#' + idArea).val(contentText);
+    $("#" + id).remove();
+    $('textarea#' + idArea).focus();
+  }
+  function hideCmt(e) {
+    e.preventDefault();
+    var parentID = $(e.target).parent().parent().parent().parent().parent().parent().parent();//.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    parentID.hide();
+    var moreCmt = 'moreComments-' + parentID.parent().attr("id").split("-")[1];
+    var numbercmt = parseInt($("#" + moreCmt).attr("class")) + 1;
+    $("#" + moreCmt).attr("class", numbercmt);
+    $("#" + moreCmt).text('Hide comments (' + numbercmt + ')');
   }
